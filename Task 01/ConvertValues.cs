5f2909946bce2ffd3cb7c5527ecd79e3f3301e94
PetharaGuruge <135795367+PetharaGuruge@users.noreply.github.com>
2023-06-09 15:53:40 +0530

    Lab04 Question01 Task01

---
+++ Task 01/ConvertValues.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_01
+{
+    class ConvertValues
+    {
+        public void KilometerTOMeter()
+        {
+            Console.WriteLine("Enter KM Value: ");
+            int km = int.Parse(Console.ReadLine());
+
+            int m = km * 1000;
+
+            Console.WriteLine("Meter Value is " + m);
+        }
+    }
+}
