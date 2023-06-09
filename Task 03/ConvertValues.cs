73547f88eead48db39b2d0a549fecaab0f2fe1b5
PetharaGuruge <135795367+PetharaGuruge@users.noreply.github.com>
2023-06-09 16:07:05 +0530

    Lab04 Question01 Task03

---
+++ Task 03/ConvertValues.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_03
+{
+    class ConvertValues
+    {
+        public int KilometerTOMeter(int km)
+        {
+
+            int m = km * 1000;
+
+            return m; 
+        }
+    }
+}
---
+++ Task 03/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter KM Value: ");
+            int km = int.Parse(Console.ReadLine());
+
+            ConvertValues objVal = new ConvertValues();
+
+            int result = objVal.KilometerTOMeter(km);
+
+            Console.WriteLine("Meter Value is " + result);
+
+            Console.ReadKey();
+        }
+    }
+}
