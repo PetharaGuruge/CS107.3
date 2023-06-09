c8d0f924e62ff522169d2e41b10056bde0f041ca
PetharaGuruge <135795367+PetharaGuruge@users.noreply.github.com>
2023-06-09 16:37:25 +0530

    Lab04 Question02

---
+++ Question 02/FindValues.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_02
+{
+    class FindValues
+    {
+        public double findArea(double r)
+        {
+            double a = 3.14 * r*r;  
+
+            return a;
+        }
+
+        public double findCircumference(double r)
+        {
+            double c = 2 * 3.14 * r;
+
+            return c;
+        }
+    }
+}
---
+++ Question 02/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the radius of the circle: ");
+            double r = double.Parse(Console.ReadLine());
+
+            FindValues objVal = new FindValues();
+
+            double a = objVal.findArea(r);
+            double c = objVal.findCircumference(r);
+
+            Console.WriteLine("Area: " + a);
+            Console.WriteLine("Circumference: " + c);
+
+            Console.ReadKey();
+        }
+    }
+}
