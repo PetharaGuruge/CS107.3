05314af20d48308d23af44d09c6b8c3053f14c1d
PetharaGuruge <135795367+PetharaGuruge@users.noreply.github.com>
2023-07-27 15:56:19 +0530

    Lab04 Question07

---
+++ Question 07/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int s1, s2; 
+            Console.WriteLine("Enter the size of the array: ");
+            s1 = int.Parse(Console.ReadLine());
+            s2 = int.Parse(Console.ReadLine());
+
+            VectorScalar objVal = new VectorScalar(s1, s2);
+        }
+    }
+}
---
+++ Question 07/VectorScalar.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_07
+{
+    class VectorScalar
+    {
+        public VectorScalar(int s1, int s2) { 
+            int i, j;
+            int[] arr1 = new int[s1];
+            int[] arr2 = new int[s2];
+
+            for (i = 0; i < s1; i++)
+            {
+                Console.Write($"Enter an element at position ({i}): ");
+                arr1[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            for (j = 0; j < s2; j++)
+            {
+                Console.Write($"Enter an element at position ({j}): ");
+                arr2[j] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+
+        private void ScalarSum(int arr1, int arr2, int s1, int s2)
+        {
+            int ss1 = 0, ss2 = 0;
+
+            for (int i = 0; i < s1; i++)
+            {
+                ss1 = ss1 + arr1[i];
+            }
+
+
+        }
+    }
+}
