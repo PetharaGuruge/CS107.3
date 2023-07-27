cbe45ece2189df0bea20664440974c5f8410959d
PetharaGuruge <135795367+PetharaGuruge@users.noreply.github.com>
2023-07-27 15:51:23 +0530

    Lab04 Question03

---
+++ Question 03/CalculateValues.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_03
+{
+    class CalculateValues
+    {
+        public int Addition(int num1, int num2)
+        {
+            int ans = num1 + num2;
+            return ans;
+        }
+
+        public int Substraction(int num1, int num2)
+        {
+            int ans = num1 - num2;
+            return ans;
+        }
+
+        public int Multiplication(int num1, int num2)
+        {
+            int ans = num1 * num2;
+            return ans;
+        }
+
+        public float Division(int num1, int num2)
+        {
+            float ans = num1 - num2;
+            return ans; 
+        }
+    }
+}
