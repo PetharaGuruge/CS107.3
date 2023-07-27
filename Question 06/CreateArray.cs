90898b3d992200e2b1de0d0b01ac288fdb6ad885
PetharaGuruge <135795367+PetharaGuruge@users.noreply.github.com>
2023-07-27 15:54:58 +0530

    Lab04 Question06

---
+++ Question 06/CreateArray.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_06
+{
+    class CreateArray
+    {
+        public CreateArray(int size) {
+            int i;
+            int []arr = new int[size];
+
+            for (i = 0; i < size; i=i+2)
+            {
+                Console.WriteLine("Enter an integer: ");
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.Write($"Array = [ ");
+            for (i = 0; i < size; i++)
+            {
+                Console.Write($" {arr[i]},");
+            }
+            Console.Write($"]");
+        }
+    }
+}
---
+++ Question 06/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size;
+
+            Console.WriteLine("Enter the size of the array: ");
+            size = int.Parse(Console.ReadLine());
+
+            CreateArray objVal = new CreateArray(size);
+            Console.ReadKey();
+        }
+    }
+}
