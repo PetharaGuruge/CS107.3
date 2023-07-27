b71c119612cd17d58ed7a924737a5f7ae9e4a8d8
PetharaGuruge <135795367+PetharaGuruge@users.noreply.github.com>
2023-07-27 15:52:57 +0530

    Lab04 Question04

---
+++ Question 04/Class.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_04
+{
+    class Class
+    {
+        private void sayHello()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Question 04/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class objSay = new Class();
+            objSay.sayHello();
+
+            //a public method can be accessed and called by any other classes 
+            //but a private method can only be called the class that it is contained in 
+        }
+    }
+}
