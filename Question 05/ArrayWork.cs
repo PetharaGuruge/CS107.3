df97c407839488ed105f974357f1403b4f1f5f45
PetharaGuruge <135795367+PetharaGuruge@users.noreply.github.com>
2023-07-27 15:53:45 +0530

    Lab04 Question05

---
+++ Question 05/ArrayWork.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_05
+{
+    class ArrayWork
+    {
+        public int minVal(int arr)
+        {
+            int i, minVal; 
+
+            minVal = arr[0];
+
+            for (i = 0; i < 10; i++)
+            {
+
+            }
+        }
+    }
+}
---
+++ Question 05/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            int[] arr = new int[10];
+
+            for (i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Enter an integer: ");
+                arr[i] = int.Parse(Console.ReadLine());
+
+                ArrayWork objVal = new ArrayWork();
+
+            }
+        }
+    }
+}
