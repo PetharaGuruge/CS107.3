4c7b52150670b303dbeb0a365f511eec20bcf160
PetharaGuruge <135795367+PetharaGuruge@users.noreply.github.com>
2023-07-29 13:44:25 +0530

    Lab05 Question 01

---
+++ Question01/Animal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question01
+{
+    class Animal
+    {
+        public void Pup()
+        {
+            Console.Write("I am an Animal");
+        }
+    }
+}
---
+++ Question01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal myAnimal = new Animal();
+            Dog myDog = new Dog();
+
+            myAnimal.Pup();
+            myDog.Pup();
+
+            Console.ReadLine();
+        }
+    }
+}
